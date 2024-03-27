2bc0fcba5d29821d19b3789512b3eaf53cd358e7
zbinxp <zbinxp@gmail.com>
2024-03-27 11:31:41 -0400

    update

--- src/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-router.post('/register', userController.registerUser);
-router.post('/login', userController.loginUser);
-router.put('/:username', userController.updateUserProfile);
-
-module.exports = router;
